80074e7915a0f0a4cd69a719804615ecdd3e277f
mahadevhim-creator <shivmahadevhim@gmail.com>
2026-02-27 22:15:49 +0530

    setup github pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/shresth-ornaments',
+  assetPrefix: '/shresth-ornaments/',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
